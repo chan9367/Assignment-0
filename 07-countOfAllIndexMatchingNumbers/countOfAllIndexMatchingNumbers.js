07853adc06970c80ab57b37c655c638961a3400e
chan9367 <61572013+chan9367@users.noreply.github.com>
2020-05-25 01:06:04 -0400

    Update countOfAllIndexMatchingNumbers.js

--- 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
+++ 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
@@ -1,5 +1,10 @@
 function countOfAllIndexMatchingNumbers(nums) {
   // Insert code here;
+  var x=0;
+  for(var i=0;i<nums.length;i++){
+    if(nums[i]==i) x++;
+  }
+  console.log(x);
 }
 
 // Do not edit this line;
