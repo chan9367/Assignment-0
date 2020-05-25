f76a5667e7afd8cdf8743d072e2f97eadef75381
chan9367 <61572013+chan9367@users.noreply.github.com>
2020-05-24 21:30:51 -0400

    Update titleCaseEdit.js

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,13 @@
 function titleCaseEdit(title) {
-  // Insert code here;
+    // Insert code here;
+  var newtitle="";
+  newtitle+=title[0].toUpperCase();
+  for(var x=1; x<title.length; x++){
+    if(title[x-1]===" ")newtitle+=title[x].toUpperCase();
+    else newtitle+=title[x];
+  }
+  title=newtitle;
+  console.log(title);
 }
 
 // Do not edit this line;
