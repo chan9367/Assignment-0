8f9dcfd3b02834674cc750c036e56af5df401894
chan9367 <61572013+chan9367@users.noreply.github.com>
2020-05-25 01:12:12 -0400

    Update sumOfAllOddNumbers.js

--- 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
+++ 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
@@ -1,5 +1,10 @@
 function sumOfAllOddNumbers(nums) {
   // Insert code here;
+  var x=0;
+  for(var i=0;i<nums.length;i++){
+    if(nums[i]%2!=0) x++;
+  }
+  console.log(x);
 }
 
 // Do not edit this line;
