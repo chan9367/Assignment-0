1e7ce31330cbbe7dda64a918b58e0912bd7e46ab
chan9367 <61572013+chan9367@users.noreply.github.com>
2020-05-24 22:15:52 -0400

    Update countOfAllBooleansAndStrings.js

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,5 +1,10 @@
 function countOfAllBooleansAndStrings(arr) {
   // Insert code here;
+  var x=0;
+  for(var i=0;i<arr.length;i++){
+    if(arr[i]==true||arr[i]==false||typeof arr[i]=="string") x++;
+  }
+  console.log(x);
 }
 
 // Do not edit this line;
