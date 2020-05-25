4d874f0a7d27be1052d006a315e9a267f0165d2b
chan9367 <61572013+chan9367@users.noreply.github.com>
2020-05-24 22:02:20 -0400

    Update countOfAllBooleans.js

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,5 +1,10 @@
 function countOfAllBooleans(arr) {
   // Insert code here;
+  var x=0;
+  for(var i=0;i<arr.length;i++){
+    if(arr[i]==true||arr[i]==false) x++;
+  }
+  console.log(x);
 }
 
 // Do not edit this line;
