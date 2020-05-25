c1319e73ef61d4f3587a5609492d648c5eea2432
chan9367 <61572013+chan9367@users.noreply.github.com>
2020-05-25 01:09:25 -0400

    Update sumOfAllEvenNumbers.js

--- 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
+++ 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
@@ -1,5 +1,10 @@
 function sumOfAllEvenNumbers(nums) {
   // Insert code here;
+  var x=0;
+  for(var i=0;i<nums.length;i++){
+    if(nums[i]%2==0) x++;
+  }
+  console.log(x);
 }
 
 // Do not edit this line;
