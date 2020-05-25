118753b477fd813e3a1c59403b9c9f474ca7e212
chan9367 <61572013+chan9367@users.noreply.github.com>
2020-05-25 00:59:48 -0400

    Update countOfAllNumbersSmallerThanTarget.js

--- 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
+++ 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
@@ -1,5 +1,10 @@
 function countOfAllNumbersSmallerThanTarget(nums, target) {
   // Insert code here;
+  var x=0;
+  for(var i=0;i<nums.length;i++){
+    if(nums[i]<target) x++;
+  }
+  console.log(x);
 }
 
 // Do not edit this line;
