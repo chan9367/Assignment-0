58bfbb2f79ab9da78de5dad5459ab4f71e2b2133
chan9367 <61572013+chan9367@users.noreply.github.com>
2020-05-25 01:25:05 -0400

    Update sumOfMinimumAndMaximum.js

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,5 +1,13 @@
 function sumOfMinimumAndMaximum(nums) {
   // Insert code here;
+  var largest=nums[0];
+  var smallest=nums[0];
+  for(var i=0;i<nums.length;i++){
+    if(nums[i]<=smallest) smallest=nums[i];
+    else if (nums[i]>=largest) largest=nums[i];
+  }
+  var sum=largest+smallest;
+  console.log(sum);
 }
 
 // Do not edit this line;
