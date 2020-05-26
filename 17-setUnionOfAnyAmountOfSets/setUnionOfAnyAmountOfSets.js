19d6d27c4f5bd3f0ef655de9682c4f3c941b5081
chan9367 <61572013+chan9367@users.noreply.github.com>
2020-05-26 01:33:11 -0400

    Update setUnionOfAnyAmountOfSets.js

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,12 @@
 function setUnionOfAnyAmountOfSets(...args) {
   // Insert code here;
+  let x = new Set();
+  for (var i=0;i<args.length;i++){
+    for (var j of args[i]){
+      x.add(j);
+    }
+  }
+  console.log(x);
 }
 
 // Do not edit this line;
