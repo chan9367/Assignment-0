532c8db68eb8384ef4ec84b211f2b329938d8ec1
chan9367 <61572013+chan9367@users.noreply.github.com>
2020-05-26 01:53:47 -0400

    Update pairSum.js

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,18 @@
 function pairSum(nums, target) {
   // Insert code here;
+  var x=false;
+  try{
+    if (nums.length<2) err;
+    for(var i=0;i<nums.length;i++){
+      for(var j=i+1;j<nums.length;j++){
+        if(nums[i]+nums[j]==target)x=true;
+      }
+    }
+    console.log(x);
+  }
+  catch(err){
+    console.log("ERROR: nums array length needs to be 2 or more")
+  }
 }
 
 // Do not edit this line;
