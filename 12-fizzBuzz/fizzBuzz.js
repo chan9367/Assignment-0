fab19c914953b723aa538f514bd651b937e71925
chan9367 <61572013+chan9367@users.noreply.github.com>
2020-05-26 00:06:41 -0400

    Update fizzBuzz.js

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,5 +1,13 @@
 function fizzBuzz(start, end) {
   // Insert code here;
+  var nums = [start];
+  for(var i=1;i<end;i++){
+    nums[i]=start+i;
+    if(nums[i]%3==0&&nums[i]%5==0) nums[i]="FizzBuzz";
+    else if (nums[i]%3==0) nums[i]="Fizz";
+    else if (nums[i]%5==0) nums[i]="Buzz";
+  }
+  console.log(nums);
 }
 
 // Do not edit this line;
