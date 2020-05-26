e30d983404d7d967186ce229e0569cb501a94d21
chan9367 <61572013+chan9367@users.noreply.github.com>
2020-05-26 00:28:39 -0400

    Update frequencyCounter.js

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,13 @@
 function frequencyCounter(word) {
   // Insert code here;
+  var x = {};
+  for(var i=0;i<word.length;i++){
+    x[word[i]]=0;
+  }
+  for(var i=0;i<word.length;i++){
+    x[word[i]]++;
+  }
+  console.log(x);
 }
 
 // Do not edit this line;
