1409c7abe343ff9b641c632fa46d21f0e10700d2
chan9367 <61572013+chan9367@users.noreply.github.com>
2020-05-26 00:57:44 -0400

    Update productOfAnyAmountOfNumbers.js

--- 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -1,5 +1,10 @@
 function productOfAnyAmountOfNumbers(...args) {
   // Insert code here;
+  var product=args[0];
+  for(var i=1;i<args.length;i++){
+    product=product*args[i];
+  }
+  console.log(product);
 }
 
 // Do not edit this line;
