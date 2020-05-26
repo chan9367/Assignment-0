bed861e75285a278a56b59bf0ea499b4a99bd8e8
chan9367 <61572013+chan9367@users.noreply.github.com>
2020-05-26 02:31:13 -0400

    Update binarySearch.js

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -5,6 +5,16 @@ class MySolution {
 
   binarySearch(nums, target) {
     // Insert code here;
+    var left=0;
+    var right=nums.length-1;
+    while(left<=right){
+      var x = Math.floor(left+right/2);
+      if(nums[x]<target) left=x+1;
+      else if(nums[x]>target) right=x-1;
+      else if(nums[x]==target){this.flag=true; left=right+1;}
+      else return this.flag=false;
+    }
+    console.log(this.flag);
   }
 }
 
