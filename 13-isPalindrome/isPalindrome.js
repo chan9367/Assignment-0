ccf052e30f2e4bc468b133d525fef8e0e2cea0ec
chan9367 <61572013+chan9367@users.noreply.github.com>
2020-05-26 00:13:15 -0400

    Update isPalindrome.js

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,5 +1,10 @@
 function isPalindrome(word) {
   // Insert code here;
+  var x=true;
+  for(var i=0;i<word.length;i++){
+    if(word[i]!=word[word.length-1-i]) x=false;
+  }
+  console.log(x);
 }
 
 // Do not edit this line;
